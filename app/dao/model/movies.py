e081465ab2d636034a35f465994684a28c5498b4
Maksim Gostev <teranys1986@gmail.com>
2023-02-06 12:45:31 +0300

    подготовка

---
+++ app/dao/model/movies.py
@@ -0,0 +1,33 @@
+from marshmallow import Schema, fields
+
+
+class Movie(db.Model):
+    __tablename__ = 'movie'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    description = db.Column(db.String(255))
+    trailer = db.Column(db.String(255))
+    year = db.Column(db.Integer)
+    rating = db.Column(db.Float)
+    genre_id = db.Column(db.Integer, db.ForeignKey("genre.id"))
+    genre = db.relationship("Genre")
+    director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
+    director = db.relationship("Director")
+
+
+class MovieSchema(Schema):
+    id = fields.Int(dump_only=True)
+    title = fields.Str()
+    description = fields.Str()
+    trailer = fields.Str()
+    year = fields.Int()
+    rating = fields.Float()
+    genre_id = fields.Int()
+    genre = fields.Method('get_genre_name')
+    director_id = fields.Int()
+    director = fields.Method('get_director_name')
+
+    def get_director_name(self, movies):
+        return movies.director.name
+    def get_genre_name(self, movies):
+        return movies.genre.name
